680e6615a0276bf85a0b768e9fd26d2cadcddb16
Wanderson Alves do Nascimento <wanderson.alves.n@outlook.com>
2025-05-05 00:17:23 -0300

    styles: Adding hover effect

--- js/home.js
+++ js/home.js
@@ -10,3 +10,50 @@ inputSearch.addEventListener("focusout", () => {
     inputSearch.classList.add('hidden');
   }
 })
+
+const sliderList = document.querySelector('.slider-list');
+
+let isPressed = false;
+let startX = 0;
+let scrollLeft = 0;
+
+sliderList.addEventListener('mousedown', (e) => {
+  isPressed = true;
+  startX = e.pageX - sliderList.offsetLeft
+  scrollLeft = sliderList.scrollLeft;
+})
+
+sliderList.addEventListener('mouseup', () => isPressed = false)
+sliderList.addEventListener('mouseleave', () => isPressed = false)
+
+sliderList.addEventListener('mousemove', (e) => {
+  if (!isPressed) {
+    return;
+  }
+
+  const x = e.pageX - sliderList.offsetLeft;
+  const move = (x - startX)
+  console.log(x)
+  sliderList.scrollLeft = scrollLeft - move;
+
+})
+
+sliderList.addEventListener('touchstart', (e) => {
+  isPressed = true;
+  startX = e.touches[0].pageX - sliderList.offsetLeft
+  scrollLeft = sliderList.scrollLeft;
+})
+
+sliderList.addEventListener('touchend', () => isPressed = false)
+sliderList.addEventListener('touchcancel', () => isPressed = false)
+
+sliderList.addEventListener('touchmove', (e) => {
+  if (!isPressed) {
+    return;
+  }
+
+  const x = e.touches[0].pageX - sliderList.offsetLeft;
+  const move = (x - startX)
+  sliderList.scrollLeft = scrollLeft - move;
+
+})
